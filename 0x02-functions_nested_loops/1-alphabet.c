0d103bf3eaec40eaa0faa8f3cfe87060988e4208
Kimani7 <newtonkipesa@gmail.com>
2023-06-21 15:48:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - a programs that prints the alphabet using _putchar
+ * Return: a;ways (0)
+ */
+
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
