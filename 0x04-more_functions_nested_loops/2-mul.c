df323edb7c179a230ab7d9915521b5445c6c9308
Kimani7 <newtonkipesa@gmail.com>
2023-06-28 04:54:22 -0700

    a function that multiplies two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - a functuion that multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: multiplacation
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
