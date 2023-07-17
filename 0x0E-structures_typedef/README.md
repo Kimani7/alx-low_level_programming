a98a9cad886024944fff920197a182619055013f
Kimani7 <newtonkipesa@gmail.com>
2023-07-17 09:23:20 -0700

    a struct dog that contains the name, age and owner of adog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This dir contains tasks on C - Structures,and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H
+/**
+ * struct dog - provides info on a dog's x-tics
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this struct takes the age, name and owner of a dog
+ */
+struct dog 
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif
