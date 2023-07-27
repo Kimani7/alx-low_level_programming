ebdaa847d76b6a88dd651b5db4c938a081712bc2
Kimani7 <newtonkipesa@gmail.com>
2023-07-27 07:41:35 -0700

    a function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - a function that prints elements of a list
+ *
+ * @h: pointer to node
+ * Return: (0) nil, number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			k++;
+		}
+	}
+	return (k);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This dir contains tasks on singly linked lists
